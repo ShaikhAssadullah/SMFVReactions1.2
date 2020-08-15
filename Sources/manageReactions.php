957d1d3a2c601e8d48ea6bc69e93392c692984be
Assadullah Shaikh <57457704+TheVersatilePro@users.noreply.github.com>
2020-08-15 14:11:48 +0500

    Add files via upload

---
+++ Sources/manageReactions.php
@@ -0,0 +1,419 @@
+<?php
+
+/**
+ * @package SMF Versatile Reactions
+ * @version 1.0
+ * @author Assadullah Shaikh a.k.a Decent_946 <asadullahshaikh20@gmail.com>
+ * @copyright Copyright (c) 2020, The Versatile Pro
+ * @license https://choosealicense.com/licenses/mpl-2.0/
+ *
+ *
+ *	This file has the functions of admin template.
+ *
+ *	It loads the main templates and handles sub actions.
+ *	manageReactions()
+ *
+ *	Function to load template of Add Reactions section.
+ *	addnewReaction()
+ *
+ *	Function that is called upon submitting of add reaction's section.
+ *	addsuccessReaction()
+ *
+ *
+ *	Function to delete a reaction from db and images file.
+ *	deleteReaction()
+ *
+ *	Function to load general section.
+ *	generalReactions()
+ *
+ *  Function to load the Edit Reaction's section that contains the list of rection emojis.
+ *	editReactions()
+ *
+ *	Function to get reaction emoji's in the list.
+ *	getReactionEmojis()
+ *
+ *	Function to get Reaction Emoji's count.
+ *  countReactionEmojis()
+ *
+ *
+ */
+
+if (!defined('SMF'))
+	die('Hacking attempt...');
+
+
+//this is the function to load admin template.
+function manageReactions()
+{
+	global $context, $txt;
+
+	//lets check permisionss first..
+	isAllowedTo(array('manage_smileys', 'admin_forum'));
+
+	//here, we create the subsections..
+	$subActions = array(
+		'general' => array('generalReactions', 'manage_smileys'),
+		'edit' => array('editReactions', 'admin_forum'),
+		'add' => array('addnewReaction', 'admin_forum'),
+		'delete' => array('deleteReaction', 'admin_forum'),
+		'addsuccess' => array('addsuccessReaction', 'admin_forum'),
+	);
+
+
+	$_REQUEST['sa'] = ((isset($_REQUEST['sa']) && !empty($_REQUEST['sa'])) ? $_REQUEST['sa'] : 'general');
+
+	//are the permissions ok?
+	isAllowedTo($subActions[$_REQUEST['sa']][1]);
+
+	//load the language for strings..
+	loadLanguage('vreactions');
+
+	//set up the title
+	$context['page_title'] = $txt['vreactions_title'];
+
+	//set up the tabs data. i.e tab name and description..
+	$context[$context['admin_menu_name']]['tab_data'] = array(
+		'title' => $txt['vreactions_title'],
+		'help' => '',
+		'description' => $txt['vreactions_tabs_general_desc'],
+		'tabs' => array(
+			'general' => array(),
+			'edit' => array(
+				'description' => $txt['vreactions_tabs_edit_desc'],
+			),
+			'add' => array(
+				'description' => $txt['vreactions_tabs_add_new_desc'],
+			),
+		),
+	);
+
+	// Force the right area...
+	if ($_REQUEST['sa'] == 'edit') {
+		$context[$context['admin_menu_name']]['current_subsection'] = 'edit';
+	}
+	else if ($_REQUEST['sa'] == 'general') {
+		$context[$context['admin_menu_name']]['current_subsection'] = 'general';
+	}
+	else if ($_REQUEST['sa'] == 'add') {
+		$context[$context['admin_menu_name']]['current_subsection'] = 'add';
+	}
+	
+
+	$subActions[$_REQUEST['sa']][0]();
+}
+
+
+
+function addnewReaction() {
+
+	global $context, $txt;
+
+	//load the required template.
+	loadTemplate('manageReactions');
+
+	//load the language for strings..
+	loadLanguage('vreactions');
+
+	$context['page_title'] = $txt['vreactions_tabs_add_new'];
+
+	//load the required template
+	$context['sub_template'] = 'add_new_reactions';
+
+}
+
+function addsuccessReaction() {
+
+	global $context, $smcFunc, $db_prefix, $settings, $boarddir, $txt;
+
+	//load the language for strings..
+	loadLanguage('vreactions');
+
+	//set the page title
+	$context['page_title'] = $txt['vreactions_tabs_add_success'];
+
+	// If nothing was chosen to delete (shouldn't happen, but meh)
+	if ( (!isset($_POST['emoji_title'])) || (!isset($_FILES['emoji_file'])) ) fatal_error($txt['vreactions_error_no_data']);
+
+	//defining allowed types and some disabled files for security.
+	$allowedTypes = array('gif');
+	$disabledFiles = array('con', 'com1', 'com2', 'com3', 'com4', 'prn', 'aux', 'lpt1', '.htaccess', 'index.php');
+	$allok = true;
+
+	// Sorry, no spaces, dots, or anything else but letters allowed.
+	$_FILES['emoji_file']['name'] = preg_replace(array('/\s/', '/\.[\.]+/', '/[^\w_\.\-]/'), array('_', '.', ''), $_FILES['emoji_file']['name']);
+
+	// We only allow image files - it's THAT simple - no messing around here...
+	if (!in_array(strtolower(substr(strrchr($_FILES['emoji_file']['name'], '.'), 1)), $allowedTypes)) {
+		fatal_error($txt['vreactions_error_invalid_image']);
+		$allok = false;
+	}
+
+	// We only need the filename...
+	$emojiName = basename($_FILES['emoji_file']['name']);
+	$explode = explode(".", $emojiName);
+	$emoji_name_without_ext = $explode[0];
+
+	// Make sure they aren't trying to upload a nasty file - for their own good here!
+	if (in_array(strtolower($emojiName), $disabledFiles)) {
+		fatal_error($txt['vreactions_error_invalid_file']);
+		$allok = false;
+	}
+
+	//so, we have a proper image now. Defining it's upload directroy.
+	$emoji_loc = $boarddir. '/Themes/default/images/reactions/' .$emojiName;
+
+	//oh oh.. we need to check if it exists previously..
+	if (file_exists($emoji_loc)){
+		fatal_error($txt['vreactions_error_file_exists']);	
+		$allok = false;
+	} 
+
+	//so all is okay. let's upload the image and insert it in database as well.
+	if ($allok === true ) {
+		//so everything is fine now. Just upload the file.
+		move_uploaded_file($_FILES['emoji_file']['tmp_name'], $emoji_loc);
+		@chmod($emoji_loc, 0644);
+
+		$smcFunc['db_insert']('insert',
+			'{db_prefix}v_reactions_emoji',
+			array(
+				'emoji_name' => 'string', 'emoji_title' => 'string',
+			),
+			array(
+				$emoji_name_without_ext, $_POST['emoji_title'],
+			),
+			array(
+				'emoji_name', 'emoji_title',
+			)
+		);
+	}
+
+	redirectexit('action=admin;area=vreactions;sa=edit;added;emoji=' .$emoji_name_without_ext);
+}
+
+function deleteReaction() {
+
+	global $context, $smcFunc, $db_prefix, $boarddir, $txt;
+
+	//load the language for strings..
+	loadLanguage('vreactions');
+
+	$context['page_title'] = $txt['vreactions_tabs_delete'];
+
+	// If nothing was chosen to delete (shouldn't happen, but meh)
+	if (!isset($_REQUEST['delete_emoji_id']) || !isset($_REQUEST['delete_emoji_name'])) 
+		fatal_error($txt['vreactions_error_no_data']);
+
+	// We have to generate the IDs and Emoji names for respective emoji from the given array.
+	foreach ($_REQUEST['delete_emoji_id'] as $key => $value)
+		$_REQUEST['delete_emoji_id'][$key] = (int) $value;
+
+	foreach ($_REQUEST['delete_emoji_name'] as $key => $value)
+		$_REQUEST['delete_emoji_name'][$key] = (string) $value;
+
+	//so, we have a proper image now. Defining it's upload directroy.
+	$emoji_loc = $boarddir. '/Themes/default/images/reactions/' .$_REQUEST['delete_emoji_name']. '.gif';
+
+	//we need to inform if the file doesn't exist.
+	$query = $smcFunc['db_query']('',
+		'SELECT * FROM {db_prefix}v_reactions_emoji 
+		WHERE emoji_id IN ({array_int:id_emoji}) 
+		OR emoji_name IN ({array_string:name_emoji})',
+		array(
+			'id_emoji' => $_REQUEST['delete_emoji_id'],
+			'name_emoji' => $_REQUEST['delete_emoji_name'],
+		)
+	);
+
+	if (!file_exists($emoji_loc) && (!$query)) {
+
+		$smcFunc['db_free_result']($query);
+		fatal_error( $txt['vreactions_error_file_not_found'], '- path:'.$emoji_loc. ' and ID ' .$_REQUEST['delete_emoji_id']);
+	}
+	else if (file_exists($emoji_loc) && (!$query)) {
+
+		unlink($emoji_loc);
+	}
+	else if (!file_exists($emoji_loc) && ($query)) {
+
+		$smcFunc['db_query']('',
+			'DELETE FROM {db_prefix}v_reactions_emoji
+			WHERE emoji_id IN ({array_int:id_emoji})',
+			array(
+				'id_emoji' => $_REQUEST['delete_emoji_id'],
+			)
+		);
+	}
+	else {
+		//if file is present, delete from DB and from images directory.
+		$smcFunc['db_query']('',
+			'DELETE FROM {db_prefix}v_reactions_emoji
+			WHERE emoji_id IN ({array_int:id_emoji})',
+			array(
+				'id_emoji' => $_REQUEST['delete_emoji_id'],
+			)
+		);
+		
+		unlink($emoji_loc);
+	}
+
+
+	$smcFunc['db_free_result']($query);
+	//we don't want the user to stay on this action.. right?
+	redirectexit('action=admin;area=vreactions;sa=edit;deleted;id=' .$_REQUEST['delete_emoji_id']);
+}
+
+function generalReactions() {
+
+	//load the required template.
+	loadTemplate('manageReactions');
+
+}
+
+function editReactions() {
+
+	global $context, $sourcedir, $txt;
+
+	//load the language for strings..
+	loadLanguage('vreactions');
+
+	//set up the title
+	$context['page_title'] = $txt['vreactions_tabs_edit'];
+
+	// We need this files
+	require_once($sourcedir . '/Subs-List.php');
+
+	//okay, set upt the table to display the reaction smiley's data..
+	$listOptions = array(
+		'id' => 'vreactions',
+		'title' => $txt['vreactions_tabs_edit_list'],
+		'items_per_page' => 15,
+		'base_href' => '?action=admin;area=vreactions;sa=edit',
+		'default_sort_col' => 'emoji_id',
+		'get_items' => array(
+			'function' => 'getReactionEmojis',
+		),
+		'get_count' => array(
+			'function' => 'countReactionEmojis',
+		),
+		'no_items_label' => $txt['vreactions_error_no_reactions'],
+		'no_items_align' => 'center',
+		'columns' => array(
+			'emoji_id' => array(
+				'header' => array(
+					'value' => $txt['vreactions_emoji_id'],
+					'class' => 'first_th',
+				),
+				'data' => array(
+					'db' => 'emoji_id',
+					'class' => 'centertext',
+				),
+				'sort' => array(
+					'default' => 'emoji_id DESC',
+					'reverse' => 'emoji_id',
+				),
+			),
+			'emoji_img' => array(
+				'header' => array(
+					'value' => $txt['vreactions_emoji'],
+					'class' => 'centertext',
+				),
+				'data' => array(
+					'function' => create_function('$row', 
+						'global $settings; 
+						return (\'<img width="26px" height="26px" src="\'. $settings[\'default_images_url\']. \'/reactions/\'. $row[\'emoji_name\']. \'.gif" alt="\'. $row[\'emoji_title\']. \'" title="\'. $row[\'emoji_title\']. \'" />\');'
+					),
+					'class' => 'centertext',
+				),
+			),
+			'emoji_name' => array(
+				'header' => array(
+					'value' => $txt['vreactions_emoji_name'],
+					'class' => 'centertext',
+				),
+				'data' => array(
+					'db' => 'emoji_name',
+					'class' => 'centertext',
+				),
+			),
+			'emoji_title' => array(
+				'header' => array(
+					'value' => $txt['vreactions_emoji_desc'],
+					'class' => 'centertext',
+				),
+				'data' => array(
+					'db' => 'emoji_title',
+					'class' => 'centertext',
+				),
+			),
+			'emoji_delete' => array(
+				'header' => array(
+					'value' => $txt['vreactions_emoji_delete']. ' <input type="checkbox" onclick="invertAll(this, this.form, \'delete_emoji_id[]\');" class="input_check" />',
+					'class' => 'last_th',
+				),
+				'data' => array(
+					'sprintf' => array(
+						'format' => '<input type="checkbox" name="delete_emoji_id[]" value="%d" class="check" />
+						<input type="hidden" name="delete_emoji_name[]" value="%s" />',
+						'params' => array(
+							'emoji_id' => false,
+							'emoji_name' => false,
+						),
+					),
+					'class' => 'centertext',
+				),
+			),
+		),
+		'form' => array(
+			'href' => '?action=admin;area=vreactions;sa=delete',
+		),
+		'additional_rows' => array(
+			array(
+				'position' => 'below_table_data',
+				'value' => '<input type="submit" size="18" value="' .$txt['vreactions_emoji_delete']. '" class="button_submit" onclick="confirm(\'' .(string)$txt['vreactions_delete_confirm']. '\');" />',
+			),
+		),
+	);
+
+	// Let's finishem
+	createList($listOptions);
+	$context['sub_template'] = 'show_list';
+	$context['default_list'] = 'vreactions';
+
+}
+
+
+function getReactionEmojis() {
+
+	global $context, $smcFunc, $db_prefix;
+
+	//get the emojis data for the list above.
+	$request = $smcFunc['db_query']('',
+		'SELECT * FROM {db_prefix}v_reactions_emoji'
+	);
+
+	// Return the data
+	$context['reactions_list'] = array();
+	while ($row = $smcFunc['db_fetch_assoc']($request))
+		$context['reactions_list'][] = $row;
+		$smcFunc['db_free_result']($request);
+
+	return $context['reactions_list'];
+}
+
+
+function countReactionEmojis() {
+
+	global $smcFunc, $db_prefix;
+
+		// Count the items for the list above.
+		$items = $smcFunc['db_query']('', '
+			SELECT emoji_id
+			FROM {db_prefix}v_reactions_emoji'
+		);
+
+		return $smcFunc['db_num_rows']($items);	
+		$smcFunc['db_free_result']($items);
+}
+
+?>
---
+++ Sources/subs-manageReactions.php
@@ -0,0 +1,421 @@
+<?php
+
+/**
+ * @package SMF Versatile Reactions
+ * @version 1.0
+ * @author Assadullah Shaikh a.k.a Decent_946 <asadullahshaikh20@gmail.com>
+ * @copyright Copyright (c) 2020, The Versatile Pro
+ * @license https://choosealicense.com/licenses/mpl-2.0/
+ */
+
+if (!defined('SMF'))
+	die('Hacking attempt...');
+
+
+/**
+*Hooks Functions.
+*/
+function addreactionactions(&$actionArray) {
+
+	//an action to call for reactions
+	$actionArray['callreactions'] = array('manageReactions.php', 'callReactions');
+}
+
+function addreactionadminsection(&$admin_areas) {
+
+	global $txt;
+	
+	//load the language for strings..
+	loadLanguage('vreactions');
+
+	//set up admin area
+	$admin_areas['layout']['areas']['vreactions'] = array (
+		'label' => 'vreactions',
+		'file' => 'manageReactions.php',
+		'function' => 'manageReactions',
+		'icon' => 'administration.gif',
+		'subsections' => array(
+			'general' => array($txt['vreactions_tabs_general'], 'manage_smileys'),
+			'edit' => array($txt['vreactions_tabs_edit'], 'admin_forum'),
+			'add' => array($txt['vreactions_tabs_add_new'], 'admin_forum'),
+		),
+	);
+}
+
+function loadreactionscripts() {
+
+	global $context, $settings;
+
+	// Load the JS and CSS in topics section.
+	if (isset($_REQUEST['topic']))
+	{
+		$context['html_headers'] .= '
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+			<link rel="stylesheet" type="text/css" href="' .$settings['default_theme_url']. '/css/vreactions.css" />
+			<script type="text/javascript" src="' .$settings['default_theme_url']. '/scripts/vreactions.js"></script>';
+	}
+}
+
+function updateModSettings(&$config_vars) {
+
+	//to update modification settings
+	$config_vars = array(
+		//versatile reactions
+		array('check', 'vreactions_revert_enabled'),
+	);
+}
+
+/**
+*Versatile Reaction Functions.
+*
+*	function to load the reactions in display.template.php
+*	loadreactionemojis($msg_id, $member_id)
+*
+*	function that is called when an emoji is clicked. to add the reaction in db.
+*	addreaction($msg_id, $emoji_id, $member_id)
+*
+*	function th remove the casted reaction previosuly.
+*   removeReaction($msg_id, $emoji_id, $member_id) 
+*
+*	This function is to get the name of user's who have reacted.
+*	getwhoreacted($msg_id, $emoji_id)
+*
+*	This function isto get the reaction count.
+*	function getreactioncount($msg_id, $emoji_id)
+*
+*	This is the function that catches the call returned from ajax and sends back the output.
+*	callReactions()
+*
+*/
+
+function loadreactionemojis($msg_id, $member_id) {
+
+	global $context, $smcFunc, $db_prefix, $settings, $txt;
+
+	//load the language for strings..
+	loadLanguage('vreactions');
+
+	//we want to load the data (emojis) from the database
+	$request = $smcFunc['db_query']('',
+		'SELECT * FROM {db_prefix}v_reactions_emoji',
+		array(
+		)
+	);
+
+	//fetching the rows.
+	$num_rows = $smcFunc['db_num_rows']($request);
+
+	//does the data exist? if no, return nothing..
+	if($num_rows <= 0) return "";
+	//if yes, load the emojis.
+	else {
+		//load the data in html layout.
+		echo '<div class="versatile_reactions">';
+		while ( $rows = $smcFunc['db_fetch_row']($request) ) {
+
+			//$rows[0] == emoji_id -    $rows[1] == emoji_name   -  $rows[2] == emoji_title
+			$emoji_format = 'gif'; //emoji format
+
+			echo '
+				<div class="emojis_wrapper windowbg2" id="emojis_wrapper-', $rows[0], '">
+					<div class="emojis" id="emoji-', $rows[0], '-', $msg_id, '" onclick="loadreactions(', $msg_id, ', ', $rows[0], ',', $member_id, ', event);">
+					<a href=""><img src="', $settings['default_images_url'], '/reactions/', $rows[1], '.', $emoji_format, '" alt="', $rows[2], '" title="', $rows[2], '"></a>
+					</div>
+					<div class="counts" id="counts-', $rows[0], '-', $msg_id, '">
+					', getreactioncount($msg_id, $rows[0]), '
+					</div>
+				</div>
+			';
+		}
+		//resetting the rows data..
+		$smcFunc['db_data_seek']($request, 0);
+		//setting up the div to view up the name of user's who have reacted.
+		while ( $rows = $smcFunc['db_fetch_row']($request) ) {
+			echo'
+				<div class="who_reacted windowbg2" id="who_reacted-', $rows[0], '-', $msg_id, '">
+				', getwhoreacted($msg_id, $rows[0]), '
+				</div>
+			';
+		}
+		echo '</div>';
+	}
+
+	//lets free the query
+	$smcFunc['db_free_result']($request);
+}
+
+
+//this functions adds the record in the database
+function addreaction($msg_id, $emoji_id, $member_id) {
+	
+	global $context, $db_prefix, $smcFunc, $modSettings, $txt;
+
+	//load the language for strings..
+	loadLanguage('vreactions');
+
+	//is it really a member? or a guest/unknown?
+	if(!$member_id || $member_id == 0 || $context['user']['is_guest']) fatal_error($txt['vreactions_error_not_reg'], false);
+
+	//getting the query with respective msg id and emoji id.
+	$query = $smcFunc['db_query']('',
+		'SELECT * FROM {db_prefix}v_reactions 
+		WHERE msg_id = {int:id_msg} 
+		AND emoji_id = {int:id_emoji}',
+		array(
+			'id_msg' => $msg_id,
+			'id_emoji' => $emoji_id,
+		)
+	);
+
+	//we want to check if the user has previously reacted to the msg with same emoji..
+	$row = $smcFunc['db_fetch_assoc']($query);
+	$who_reacts = explode(",", $row['members_id']);
+
+	for ($x=0; $x<sizeof($who_reacts); $x++) {
+
+		if($member_id == $who_reacts[$x]) {
+			if(!empty($modSettings['vreactions_revert_enabled'])) removeReaction($msg_id, $emoji_id, $member_id);
+			else fatal_error($txt['vreactions_error_already_reacted'], false);
+			
+			return 0;
+		}
+		else continue;
+	}
+
+	//We want to check if someone else has previously reacted to that msg with same emoji. We'll just update then..
+	$check = $smcFunc['db_num_rows']($query);
+	if ( $check > 0 ) {
+
+		//now, we wanna update no of reactions and also add the user id in database so that we can see who reacted.
+		$request = $smcFunc['db_query']('',
+			'UPDATE {db_prefix}v_reactions 
+			SET members_id = CONCAT_WS(",", members_id, {int:id_member}), no_of_reacts = no_of_reacts+1 
+			WHERE msg_id = {int:id_msg} 
+			AND emoji_id = {int:id_emoji}',
+			array(
+				'id_member' => $member_id,
+				'id_msg' => $msg_id,
+				'id_emoji' => $emoji_id,
+			)
+		);
+
+		//announce if successfull/failed.
+		if ($request) return $txt['vreactions_reacted'];
+		else return $txt['vreactions_failed'];
+	}
+	else {
+		//if the query is empty, that means no one has voted to that msg with selected emoji. We'll then insert a new record.
+		$smcFunc['db_insert']('insert',
+			'{db_prefix}v_reactions',
+			array(
+				'msg_id' => 'int', 'emoji_id' => 'int', 'members_id' => 'text', 'no_of_reacts' => 'int',
+			),
+			array(
+				$msg_id, $emoji_id, $member_id, 1,
+			),
+			array(
+				'msg_id', 'emoji_id',
+			)
+		);
+
+		return $txt['vreactions_added'];
+	}
+
+	//lets free the query
+	$smcFunc['db_free_result']($query);
+
+}
+
+//this is to remove the reaction of member who has previously reacted.
+function removeReaction($msg_id, $emoji_id, $member_id) {
+
+	global $context, $db_prefix, $smcFunc, $txt;
+
+	//getting the query with respective msg id and emoji id.
+	$query = $smcFunc['db_query']('',
+		'SELECT * FROM {db_prefix}v_reactions 
+		WHERE msg_id = {int:id_msg} 
+		AND emoji_id = {int:id_emoji}',
+		array(
+			'id_msg' => $msg_id,
+			'id_emoji' => $emoji_id,
+		)
+	);
+
+	//checking if there are multiple reactions to the selected emoji.
+	$row = $smcFunc['db_fetch_assoc']($query);
+
+	if( $row['no_of_reacts'] > 1 ) {
+
+		//resetting the rows data..
+		// $smcFunc['db_data_seek']($query, 0);
+
+		$who_reacts = explode(",", $row['members_id']);
+		$delete_member = array_diff($who_reacts, array($member_id));
+		$who_reacts = implode(",", $delete_member);
+
+
+		//now, we wanna reduce no of reactions and also remove the user id in database.
+		$request = $smcFunc['db_query']('',
+			'UPDATE {db_prefix}v_reactions 
+			SET members_id = {string:id_member}, no_of_reacts = no_of_reacts-1 
+			WHERE msg_id = {int:id_msg} 
+			AND emoji_id = {int:id_emoji}',
+			array(
+				'id_member' => $who_reacts,
+				'id_msg' => $msg_id,
+				'id_emoji' => $emoji_id,
+			)
+		);
+
+		//announce if successfull/failed.
+		if ($request) return $txt['vreactions_unreacted'];
+		else return $txt['vreactions_failed'];
+	}
+	//otherwise it means there's only 1 reaction. We will delete the entry.
+	else {
+
+		$smcFunc['db_query']('',
+			'DELETE FROM {db_prefix}v_reactions
+			WHERE emoji_id = {int:id_emoji}',
+			array(
+				'id_emoji' => $emoji_id,
+			)
+		);
+
+		return $txt['vreactions_unreacted'];	
+	}
+
+	//lets free the query
+	$smcFunc['db_free_result']($query);
+}
+
+//this is to get the name of user's who have reacted.
+function getwhoreacted($msg_id, $emoji_id) {
+
+	global $context, $smcFunc, $db_prefix, $txt;
+
+	//load the language for strings..
+	loadLanguage('vreactions');
+
+	$query = $smcFunc['db_query']('',
+		'SELECT * FROM {db_prefix}v_reactions 
+		WHERE msg_id = {int:id_msg} 
+		AND emoji_id = {int:id_emoji} LIMIT 1',
+		array(
+			'id_msg' => $msg_id,
+			'id_emoji' => $emoji_id,
+		)
+	);
+
+
+	//we want to check if nobody has yet reacted..
+	$check = $smcFunc['db_num_rows']($query);
+	if ( $check == 0 ) return $txt['vreactions_empty'];
+	//we want to check if someone or more have reacted. Display there names.
+	else {
+
+		$row = $smcFunc['db_fetch_assoc']($query);
+		$who_reacts = explode(",", $row['members_id']);
+
+		$emoji_query = $smcFunc['db_query']('',
+			'SELECT * FROM {db_prefix}v_reactions_emoji 
+			WHERE emoji_id = {int:id_emoji} LIMIT 1',
+			array(
+				'id_emoji' => $emoji_id,
+			)
+		);
+
+		$emoji_data = $smcFunc['db_fetch_assoc']($emoji_query);
+		$emoji_name = $emoji_data['emoji_name'];
+
+		$data = $txt['vreactions_members_reacted']. ' ' .$emoji_name. ':';
+		$data .= '<table><tr class="windowbg2">';
+		for($x=0; $x<sizeof($who_reacts); $x++ ) {
+
+			$request = $smcFunc['db_query']('',
+				'SELECT real_name FROM {db_prefix}members 
+				WHERE id_member = {int:member_id}',
+				array(
+					'member_id' => (int)$who_reacts[$x],
+				)
+			);
+
+			$get = $smcFunc['db_fetch_assoc']($request);
+			$realname = $get['real_name'];
+			$data .= '<td class="centertext">' .$realname. ',</td>';
+			//lets free the query..
+			$smcFunc['db_free_result']($request);
+		}
+		$data .= '</tr></table>';
+		return $data;
+
+		//lets free the query..
+		$smcFunc['db_free_result']($emoji_query);
+	}
+	
+	//lets free the queries.
+	$smcFunc['db_free_result']($query);
+}
+
+
+//this is the function to get the reaction count.
+function getreactioncount($msg_id, $emoji_id) {
+
+	global $smcFunc, $db_prefix;
+
+	//getting the data from the database...
+	$vquery = $smcFunc['db_query']('',
+			'SELECT * FROM {db_prefix}v_reactions 
+			WHERE msg_id = {int:id_msg} 
+			AND emoji_id = {int:id_emoji} LIMIT 1',
+			array(
+				'id_msg' => $msg_id,
+				'id_emoji' => $emoji_id,
+			)
+		);
+
+	//checking if data even existss,
+	$check = $smcFunc['db_num_rows']($vquery);
+	if ( $check == 0 ) return "0";
+	//print if the record exists.
+	else {
+		$row = $smcFunc['db_fetch_assoc']($vquery);
+		$no_of_reacts = (int)$row['no_of_reacts'];
+
+		return $no_of_reacts;
+	}
+	$smcFunc['db_free_result']($vquery);
+}
+
+
+//This is the function that catches the call returned from ajax and sends back the output.
+function callReactions() {
+
+	global $user_info, $txt;
+
+	//load the language for strings..
+	loadLanguage('vreactions');
+	
+
+	//verify POST data for security reasons.
+	$request = file_get_contents('php://input');
+	if ($request === FALSE || empty($request)) fatal_error($txt['vreactions_error_no_data']);
+
+	if (isset($_POST['msg_id'])) {
+
+		addreaction($_POST['msg_id'], $_POST['emoji_id'], $user_info['id']);
+
+		$no_of_reactions = getreactioncount($_POST['msg_id'], $_POST['emoji_id']);
+		$who_reacted = getwhoreacted($_POST['msg_id'], $_POST['emoji_id']);
+
+		$return_arr = array('no_of_reactions' => $no_of_reactions, 'who_reacted' => $who_reacted );
+
+		header('Content-type: application/json');
+		echo json_encode($return_arr);
+	}
+
+	die();
+}
+
+?>
