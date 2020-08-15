c4fe81f344fa34c0275393e366e23939cc4dc00f
Assadullah Shaikh <57457704+TheVersatilePro@users.noreply.github.com>
2020-08-15 14:26:27 +0500

    Create vreactions.english.php

---
+++ Themes/default/languages/vreactions.english.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * @package SMF Versatile Reactions
+ * @version 1.0
+ * @author Assadullah Shaikh a.k.a Decent_946 <asadullahshaikh20@gmail.com>
+ * @copyright Copyright (c) 2020, The Versatile Pro
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
+ */
+
+//Admin Tabs
+$txt['vreactions_title'] = 'Versatile Reactions';
+$txt['vreactions_tabs_general'] = 'General';
+$txt['vreactions_tabs_general_desc'] = 'General Section of Versatile Reactions. Here you can view the developer and used tools information';
+$txt['vreactions_tabs_edit'] = 'Edit Reactions';
+$txt['vreactions_tabs_edit_desc'] = 'Settings area of Versatile Reactions. Here you can view active reactions and remove any reaction smiley.';
+$txt['vreactions_tabs_edit_list'] = 'Versatile Reactions List';
+$txt['vreactions_tabs_add_new'] = 'Add Reactions';
+$txt['vreactions_tabs_add_new_desc'] = 'This the the area where you can add new reaction smileys.';
+$txt['vreactions_tabs_add_success'] = 'Added new Reaction';
+$txt['vreactions_tabs_delete'] = 'Delete Reaction Smileys';
+$txt['vreactions_revert_enabled'] = '[SMFVReactions]Allow members to revert their reaction';
+
+//reactions list
+$txt['vreactions_emoji_id'] = 'ID';
+$txt['vreactions_emoji'] = 'Emoji';
+$txt['vreactions_emoji_name'] = 'Name';
+$txt['vreactions_emoji_desc'] = 'Title or Description';
+$txt['vreactions_emoji_delete'] = 'Delete';
+$txt['vreactions_emoji_move'] = 'Move';
+$txt['vreactions_emoji_moveup'] = 'Move Up';
+$txt['vreactions_emoji_movedown'] = 'Move Down';
+
+//general
+$txt['vreactions_reacted'] = 'Reacted.';
+$txt['vreactions_unreacted'] = 'Unreacted.';
+$txt['vreactions_failed'] = 'Failed.';
+$txt['vreactions_added'] = 'Added.';
+$txt['vreactions_empty'] = 'No reactions';
+$txt['vreactions_members_reacted'] = 'Members reacted';
+$txt['vreactions_delete_confirm'] = 'Are you sure you want to delete selected reaction smiley(s)?';
+
+//template data
+$txt['vreactions_text_author'] = 'Author';
+$txt['vreactions_text_dev'] = 'Developer';
+$txt['vreactions_dev'] = 'Assadullah Shaikh a.k.a Decent_946';
+$txt['vreactions_text_tools_used'] = 'Third Party Services';
+$txt['vreactions_text_js'] = '<a href="https://www.javascript.com/" target="_blank">JavaScript</a>';
+$txt['vreactions_text_jquery'] = '<a href="https://jquery.com/" target="_blank">Jquery</a>';
+$txt['vreactions_text_ajax'] = '<a href="https://api.jquery.com/jquery.ajax/" target="_blank">Ajax</a>';
+$txt['vreactions_text_joypixels'] = '<a href="https://www.joypixels.com/" target="_blank">JoyPixels</a>';
+$txt['vreactions_text_support'] = 'Support Information';
+$txt['vreactions_text_support_ver'] = 'Supported Versions';
+$txt['vreactions_support_ver'] = '2.0.x <br /> 2.1 RCx';
+$txt['vreactions_text_add_features'] = 'Additional Features';
+$txt['vreactions_add_features_title'] = 'Do you need additional features in this mod?';
+$txt['vreactions_add_features_desc'] = 'Such as; allowing/disallowing users or usergroups to react a post, Delete a reaction of user in a post, etc.<br /><a href="https://www.simplemachines.org/community/index.php?action=profile;u=413310" target="_blank">Contact me via PM at SMF</a> and I\'ll develop the feature for you. :)';
+$txt['vreactions_add_reaction'] = 'Add New Reaction';
+$txt['vreactions_upload'] = 'Upload Image File:';
+$txt['vreactions_text_upload'] = 'Uplaod';
+$txt['vreactions_upload_desc'] = 'Only .gif file is allowed';
+$txt['vreactions_desc'] = 'Emoji Title or Description:';
+
+//error
+$txt['vreactions_error_no_data'] = 'Error - No Post data was found. Please retry.';
+$txt['vreactions_error_invalid_image'] = 'Error - Only .gif file is allowed.';
+$txt['vreactions_error_invalid_file'] = 'Error - Invalid file. Please uplaod an image with .gif extension.';
+$txt['vreactions_error_file_exists'] = 'Error - An image with this name already exists.';
+$txt['vreactions_error_file_not_found'] = 'Error - File Not Found.';
+$txt['vreactions_error_confirm'] = 'Error - Are you sure you want to delete this reaction smiley?';
+$txt['vreactions_error_empty_table'] = 'Error - Table has no data';
+$txt['vreactions_error_not_reg'] = 'Error - You need to be a registered user to react.';
+$txt['vreactions_error_already_reacted'] = 'Error - you\'ve reacted previously.';
+$txt['vreactions_error_no_reactions'] = 'Error - No emoji\'s found in database.';
+
+// $txt[''] = '';
+// $txt[''] = '';
+// $txt[''] = '';
+// $txt[''] = '';
+// $txt[''] = '';
+
+
+?>
