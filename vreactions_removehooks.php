5647fe36b8bdcad04534e26933eb7d802d4bc5a9
Assadullah Shaikh <57457704+TheVersatilePro@users.noreply.github.com>
2020-08-15 14:06:56 +0500

    Add files via upload

---
+++ vreactions_removehooks.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @package SMF Versatile Reactions
+ * @version 1.0
+ * @author Assadullah Shaikh a.k.a Decent_946 <asadullahshaikh20@gmail.com>
+ * @copyright Copyright (c) 2020, The Versatile Pro
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
+ */
+
+	if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+		require_once(dirname(__FILE__) . '/SSI.php');
+
+	elseif (!defined('SMF'))
+		exit('<b>Error:</b> Cannot uninstall - please verify you put this in the same place as SMF\'s index.php.');
+
+
+remove_integration_function('integrate_pre_include', '$sourcedir/manageReactions.php');
+remove_integration_function('integrate_pre_include', '$sourcedir/subs-manageReactions.php');
+remove_integration_function('integrate_actions', 'addreactionactions');
+remove_integration_function('integrate_admin_areas', 'addreactionadminsection');
+remove_integration_function('integrate_load_theme', 'loadreactionscripts');
+remove_integration_function('integrate_general_mod_settings', 'updateModSettings');
+?>
